b7729b207528317ca322a42b6f78201c0b87711a
git961 <120348157+git961@users.noreply.github.com>
2025-02-21 12:14:21 +0900

    イベントライン更新
    イベントラインが上下移動するように追加。

--- GameJam2025/GameJam/Object/EventLine.cpp
+++ GameJam2025/GameJam/Object/EventLine.cpp
@@ -1,1 +1,72 @@
 #include "EventLine.h"
+#include "../Utility/PadInputManager.h"
+#include "DxLib.h"
+
+EventLine::EventLine()
+{
+	//240.0fは現在の画面の半分のサイズ
+	stop_line_bottom = 240.0f;
+	stop_line_up = 0.0f;
+	//120.0fは画面の半分のサイズのさらに半分
+	location.y = 120.0f;
+	location.x = 320.0f;
+	stop_location_y = 0;
+	is_moving_up = true;
+	is_stop = false;
+}
+
+EventLine::~EventLine()
+{
+}
+
+void EventLine::Update()
+{
+	PadInputManager* pad_input = PadInputManager::GetInstance();
+
+	if (is_stop == false)
+	{
+		//もし上の限界値までｙが来たら
+		if (stop_line_up > location.y)
+		{
+			//上に上がるのを止める
+			is_moving_up = false;
+		}
+
+		//もし下の限界値までｙが来たら
+		if (stop_line_bottom < location.y)
+		{
+			is_moving_up = true;
+		}
+
+		//もし上がって良かったら
+		if (is_moving_up == true)
+		{
+			location.y -= 2.4f;
+		}
+		else
+		{
+			location.y -= 2.4f;
+		}
+	}
+
+	if (pad_input->GetKeyInputState(XINPUT_BUTTON_B) == eInputState::ePress)
+	{
+		is_stop = true;
+		stop_location_y = (int)location.y;
+	}
+}
+
+void EventLine::Draw() const
+{
+	DrawTriangleAA(location.x, location.y, location.x + 30.0f, location.y + 30.0f, location.y - 30.0f,location.x - 30.0f, location.y - 30.0f, 0xffffff, TRUE);
+}
+
+bool EventLine::CheckStop()
+{
+	//動きが止まっていたらtrueを返す
+	if (is_stop == true)
+	{
+		return true;
+	}
+	return false;
+}
--- GameJam2025/GameJam/Object/EventLine.h
+++ GameJam2025/GameJam/Object/EventLine.h
@@ -1,5 +1,26 @@
 #pragma once
+#include "../Utility/Geometry.h"
+
 class EventLine
 {
-};
+private:
+	Vector2D location;//イベントラインの座標
+	float stop_line_up;//イベントラインが上に上がる限界値
+	float stop_line_bottom;//イベントラインが下に下がる限界値
 
+	int stop_location_y;//イベントラインの止まったｙを入れる
+
+	bool is_moving_up;//上に移動するか
+	bool is_stop;//動きを止めるか
+
+public:
+	EventLine();
+	~EventLine();
+	void Update();
+	void Draw()const;
+
+	//ラインが止まったｙ座標を返す
+	int GetLineStopY() { return stop_location_y; }
+	//動きが止まったかを返す
+	bool CheckStop();
+};
